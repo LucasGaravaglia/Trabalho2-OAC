4864756430824a528f110ac73b477dc286bbf745
Levi <arcanjolevi@gmail.com>
2021-04-15 13:41:35 -0400

    general fixes - output of instruction memory

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 package src;
 
-import src.utils.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
-		System.out.println(Binary.get32BitsStringValue(-256));
+
 		
 	}	
 }
--- src/hardware/memory/InstructionMemory.java
+++ src/hardware/memory/InstructionMemory.java
@@ -1,28 +1,48 @@
 package src.hardware.memory;
 
+import src.utils.*;
+
 /**
  * @author Levi
  */
 public class InstructionMemory {
     private String[] instructions;
+    private String readAddress; 
 
     /**
-     * 
-     * @param instructions 32 bits instructions 
+     * Constructor
+     * Each instruction per index
+     * @param instructions 32 bits instructions, String with length 32 per index
      */
     public InstructionMemory(String[] instructions){
         this.setInstructionMemory(instructions);
+        this.readAddress = "000000";
     }
 
     /**
+     * Function that update all memory replacing it
      * @param instructions 32 bits instructions 
      */
     public void setInstructionMemory(String[] instructions){
-        this.instructions = instructions;
+        this.instructions = instructions.clone();
+        for(int i = 0; i < this.instructions.length; i++){
+            if(this.instructions[i].length() != 32){
+               this.instructions[i] = src.utils.Binary.BITS_32_ZERO; 
+            }
+        }
+    }
+    
+    /**
+     * Function that clone the content of the memory
+     * @return a String array, one instruction per index
+     */
+    public String[] getMemoryClone(){
+        return this.instructions.clone();
     }
 
     /**
-     * @return String value of all instructions storaged
+     * Function convert content to String
+     * @return String value of all instructions stored
      */
     public String toString(){
         String text = "";
@@ -33,13 +53,95 @@ public class InstructionMemory {
     }
 
     /**
-     * 
-     * @param adress_32_bits 32 bits adress
-     * @return 32 bits instruction storaged
+     * Function that should be used for read a value of the memory instruction
+     * @param address_32_bits 32 bits address
+     * @return 32 bits instruction stored
      */
-    public String getInstruction(String adress_32_bits){
-        String adress_16_bits = adress_32_bits.substring(16);
-        int index = Integer.parseInt(adress_16_bits, 2);
+    public String getInstruction(String address_32_bits){
+        String address_16_bits = address_32_bits.substring(16);
+        int index = Integer.parseInt(address_16_bits, 2);
         return this.instructions[index];
+    }
+
+    /**
+     * Function that determines which address will be read
+     * @param readAddress
+     */
+    public void setReadAddress(String readAddress){
+        if(this.instructions.length >= Long.parseLong(readAddress, 2)){
+            if(readAddress.length() >= 32){
+                this.readAddress = readAddress.substring(1);
+            }else{
+                this.readAddress = readAddress;
+            }            
+        }else{
+            this.readAddress = "0";
+        }        
+    }
+
+    /**
+     * Function that represents the output 6-0 of the instruction
+     * Index of the String:        0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
+     *                             ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^ ^   ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+     * Index of the instruction:  31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0
+     * @return
+     */ 
+    public String get_6_0(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(25);
+    }
+
+    /**
+     * Function that represents the output 11-7 of the instruction
+     * @return
+     */
+    public String get_11_7(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(20, 25);
+    }
+
+    /**
+     * Function that represents the output 14-12 of the instruction
+     * @return
+     */
+    public String get_14_12(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(17, 20);
+    }
+
+    /**
+     * Function that represents the output 15-19 of the instruction
+     * @return
+     */
+    public String get_15_19(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(12, 17);
+    }
+
+    /**
+     * Function that represents the output 20-24 of the instruction
+     * @return
+     */
+    public String get_20_24(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(7, 12);
+    }
+
+    /**
+     * Function that represents the output 30,14-12
+     * @return
+     */
+    public String get_30__14_12(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value.substring(1, 2)+value.substring(17, 20);
+    }
+
+    /**
+     * Function that represents the output 31-0
+     * @return
+     */
+    public String get_31_0(){
+        String value = this.instructions[Integer.parseInt(this.readAddress, 2)];
+        return value;
     }
 }
