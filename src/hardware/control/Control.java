b2a5b2fefaa46cd0b93d3d2d18d924e55dd417a0
Levi <arcanjolevi@gmail.com>
2021-04-16 17:04:04 -0400

    First commit

---
+++ src/hardware/control/Control.java
@@ -0,0 +1,12 @@
+package src.hardware.control;
+
+public class Control {
+    private String input;
+    private String branch;
+    private String MemRead;
+    private String MemToReg;
+    private String ALUOp;
+    private String MemWrite;
+    private String ALUSrc;
+    private String RegWrite;
+}
