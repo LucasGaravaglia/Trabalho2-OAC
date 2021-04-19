67978ad7bc5c86ede53fc31705ed79545b8ca0a1
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-04-19 19:41:28 -0300

    Adição dos comentarios e da documentação da classe data

--- src/gui/data/Data.java
+++ src/gui/data/Data.java
@@ -2,12 +2,20 @@ package src.gui.data;
 
 import javax.swing.DefaultListModel;
 
+/**
+ * Class responsible for data type the interface graphic.
+ * 
+ * @author Lucas Garavaglia
+ */
 public class Data {
   private DefaultListModel<String> modelMemory;
   private DefaultListModel<String> modelRegister;
   private Boolean[] signals;
   private Integer Pc;
 
+  /**
+   * Constructor of class
+   */
   public Data() {
     this.modelMemory = new DefaultListModel<String>();
     this.modelRegister = new DefaultListModel<String>();
@@ -15,10 +23,20 @@ public class Data {
     this.Pc = 0;
   }
 
+  /**
+   * Setter Pc
+   * 
+   * @param pc String Pc
+   */
   public void setPc(String pc) {
     this.Pc = src.utils.Binary.getDecimalValue(pc);
   }
 
+  /**
+   * Setter Signals
+   * 
+   * @param signals String array the signals.
+   */
   public void setSignals(String[] signals) {
     int i = 0;
     for (String boolean1 : signals) {
@@ -27,34 +45,71 @@ public class Data {
     }
   }
 
+  /**
+   * Setter ModelRegister
+   * 
+   * @param line Line that will be added to the modelMemory
+   */
   public void setModelMemory(String line) {
     this.modelMemory.addElement(line);
   }
+
+  /**
+   * Setter ModelRegister
+   * 
+   * @param line Line that will be added to the modelRegister
+   */
 
   public void setModelRegister(String line) {
     this.modelRegister.addElement(line);
   }
 
+  /**
+   * Method responsible for clear the modelMemory.
+   */
   public void clearModelMemory() {
     this.modelMemory.clear();
   }
 
+  /**
+   * Method responsible for clear the modelRegister.
+   */
   public void clearModelRegister() {
     this.modelRegister.clear();
   }
 
+  /**
+   * Getter Pc
+   * 
+   * @return Pc
+   */
   public Integer getPc() {
     return this.Pc;
   }
 
+  /**
+   * Getter Signals
+   * 
+   * @return Signals
+   */
   public Boolean[] getSignals() {
     return this.signals;
   }
 
+  /**
+   * Getter ModelMemory
+   * 
+   * @return ModelMemory
+   */
   public DefaultListModel<String> getModelMemory() {
     return this.modelMemory;
   }
 
+  /**
+   * Getter ModelRegister
+   * 
+   * @return ModelRegister
+   */
   public DefaultListModel<String> getModelRegister() {
     return this.modelRegister;
   }
