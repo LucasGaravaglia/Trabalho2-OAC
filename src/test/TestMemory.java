10071e75384f51aacb8a20860e4b743b912e2a7c
Levi <arcanjolevi@gmail.com>
2021-04-19 20:36:48 -0400

    toString update

---
+++ src/test/TestMemory.java
@@ -0,0 +1,16 @@
+package src.test;
+
+import src.hardware.memory.*;
+
+public class TestMemory {
+    public static void testDatamemory(){
+        DataMemory m = new DataMemory();
+        String[] samples = {"00000000000000000000000000000000", "00000000000000000000000000000000"};
+        System.out.println(m.toString());
+    }
+
+    public static void testRegisters(){
+        Registers r = new Registers();
+        System.out.println(r.toString());
+    }
+}
