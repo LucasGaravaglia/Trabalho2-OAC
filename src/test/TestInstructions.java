be4552516cae8a1cf5b1d0606a7b0988fb111296
CaioslppUO <caioslppuo@gmail.com>
2021-04-22 10:17:20 -0300

    Added function to translate assembly to binary.

--- src/test/TestInstructions.java
+++ src/test/TestInstructions.java
@@ -11,7 +11,6 @@ public class TestInstructions {
                 sRs1 = src.utils.Binary.get32BitsStringValue(rs1).substring(27, 32),
                 sImm = src.utils.Binary.get32BitsStringValue(imm).substring(20, 32);
         return sImm + sRs1 + funct3 + sRd + opcode;
-        
     }
 
     private static String typeR(Integer rd, Integer rs1, Integer rs2, String funct7, String opcode, String funct3) {
@@ -19,15 +18,13 @@ public class TestInstructions {
                 sRs1 = src.utils.Binary.get32BitsStringValue(rs1).substring(27, 32),
                 sRs2 = src.utils.Binary.get32BitsStringValue(rs2).substring(27, 32);
         return funct7 + sRs2 + sRs1 + funct3 + sRd + opcode;
-        
     }
 
     private static String typeS(Integer rs1, Integer rs2, Integer imm, String opcode, String funct3) {
         String sRs1 = src.utils.Binary.get32BitsStringValue(rs1).substring(27, 32),
                 sImm = src.utils.Binary.get32BitsStringValue(imm).substring(20, 32),
                 sRs2 = src.utils.Binary.get32BitsStringValue(rs2).substring(27, 32);
-        return sImm.substring(0, 7) + sRs2 + sRs1 + funct3 + sImm.substring(7, 12) + opcode;
-        
+        return sImm.substring(0, 7) + sRs2 + sRs1 + funct3 + sImm.substring(7, 12) + opcode; 
     }
 
     private static String typeB(Integer rs1, Integer rs2, Integer imm, String opcode, String funct3) {
@@ -38,7 +35,7 @@ public class TestInstructions {
                 + sImm.substring(1, 2) + opcode;
     }
 
-    // TIPO R
+    // TYPE R
     public static String add(Integer rd, Integer rs1, Integer rs2) {
         String opcode = "0110011", funct3 = "000", funct7 = "0000000";
         return typeR(rd, rs1, rs2, funct7, opcode, funct3);
@@ -59,7 +56,7 @@ public class TestInstructions {
         return typeR(rd, rs1, rs2, funct7, opcode, funct3);
     }
 
-    // TIPO I
+    // TYPE I
     public static String addi(Integer rd, Integer rs1, Integer imm) {
         String opcode = "0010011", funct3 = "000";
         return typeI(rd, rs1, imm, opcode, funct3);
@@ -70,13 +67,13 @@ public class TestInstructions {
         return typeI(rd, rs1, imm, opcode, funct3);
     }
 
-    // TIPO S
+    // TYPE S
     public static String sw(Integer rs1, Integer rs2, Integer imm) {
         String opcode = "0100011", funct3 = "010";
         return typeS(rs1, rs2, imm, opcode, funct3);
     }
 
-    // TIPO B
+    // TYPE B
     public static String beq(Integer rs1, Integer rs2, Integer imm) {
         String opcode = "1100011", funct3 = "000";
         return typeB(rs1, rs2, imm, opcode, funct3);
@@ -85,6 +82,64 @@ public class TestInstructions {
     public static String bne(Integer rs1, Integer rs2, Integer imm) {
         String opcode = "1100011", funct3 = "001";
         return typeB(rs1, rs2, imm, opcode, funct3);
+    }
+
+    public static String translateToBinary(String asmInst) {
+        String operation;
+        Integer rd,rs1,rs2,imm;
+        String[] values;
+        String[] asm = asmInst.split(" ");
+        operation = asm[0];
+        values = asm[1].split(",");
+        switch(operation) {
+            case "add":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("x")[1]);
+                rs2 = Integer.parseInt(values[2].split("x")[1]);
+                return add(rd,rs1,rs2);
+            case "sub":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("x")[1]);
+                rs2 = Integer.parseInt(values[2].split("x")[1]);
+                return sub(rd,rs1,rs2);
+            case "and":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("x")[1]);
+                rs2 = Integer.parseInt(values[2].split("x")[1]);
+                return and(rd,rs1,rs2);
+            case "or":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("x")[1]);
+                rs2 = Integer.parseInt(values[2].split("x")[1]);
+                return or(rd,rs1,rs2);
+            case "addi":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("x")[1]);
+                imm = Integer.parseInt(values[2]);
+                return addi(rd, rs1, imm);
+            case "lw":
+                rd = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("\\(")[1].split("\\)")[0].split("x")[1]);
+                imm = Integer.parseInt(values[1].split("\\(")[0]);
+                return lw(rd, rs1, imm);
+            case "sw":
+                rs2 = Integer.parseInt(values[0].split("x")[1]);
+                rs1 = Integer.parseInt(values[1].split("\\(")[1].split("\\)")[0].split("x")[1]);
+                imm = Integer.parseInt(values[1].split("\\(")[0]);
+                return sw(rs1, rs2, imm);
+            case "beq":
+                rs1 = Integer.parseInt(values[0].split("x")[1]);
+                rs2 = Integer.parseInt(values[1].split("x")[1]);
+                imm = Integer.parseInt(values[2]);
+                return beq(rs1, rs2, imm);
+            case "bne":
+                rs1 = Integer.parseInt(values[0].split("x")[1]);
+                rs2 = Integer.parseInt(values[1].split("x")[1]);
+                imm = Integer.parseInt(values[2]);
+                return bne(rs1, rs2, imm);
+            default:
+                return "";
+        }
     }
 
 }
