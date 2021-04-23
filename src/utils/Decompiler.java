158985370ccab730ee7c491d0321da2d3ebeafde
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-04-23 10:38:23 -0300

    Criação do descompilador para interface

---
+++ src/utils/Decompiler.java
@@ -0,0 +1,173 @@
+package src.utils;
+
+import src.utils.Binary;
+
+public class Decompiler {
+  /**
+   * Receives instructions in binary and returns to instructions in assembly.
+   * 
+   * @param instructions Binary of instructions
+   */
+  public static void decompiler(String[] instructions) {
+    String[] instructionAssembly = new String[instructions.length];
+    int n = 0;
+    String opcode = new String();
+    String funct3 = new String();
+    String funct7 = new String();
+    String rd = new String();
+    String rs1 = new String();
+    String rs2 = new String();
+    String imm = new String();
+    for (String string : instructions) {
+      opcode = getOpcode(string);
+      funct3 = getFunct3(string);
+      switch (opcode) {
+      case "0110011":// tipo R
+        funct7 = getFunct7(string);
+        rd = getRd(string);
+        rs1 = getRs1(string);
+        rs2 = getRs2(string);
+        if (funct3.compareTo("000") == 0) {// sub,add
+          if (funct7.compareTo("0100000") == 0) {// sub
+            instructionAssembly[n] = "SUB X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", X" + getDecimal(rs2);
+          } else if (funct7.compareTo("0000000") == 0) {// add
+            instructionAssembly[n] = "ADD X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", X" + getDecimal(rs2);
+          }
+        } else if (funct3.compareTo("111") == 0) {// and
+          instructionAssembly[n] = "AND X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", X" + getDecimal(rs2);
+        } else if (funct3.compareTo("110") == 0) {// or
+          instructionAssembly[n] = "OR X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", X" + getDecimal(rs2);
+        }
+      case "0010011":// tipo I/addi
+        imm = getImmTypeI(string);
+        rd = getRd(string);
+        rs1 = getRs1(string);
+        instructionAssembly[n] = "ADDI X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", " + getDecimal(imm);
+      case "0000011":// tipo I/lw
+        imm = getImmTypeI(string);
+        rd = getRd(string);
+        rs1 = getRs1(string);
+        instructionAssembly[n] = "LW X" + getDecimal(rd) + ", X" + getDecimal(rs1) + ", " + getDecimal(imm);
+      case "0100011":// tipo S/sw
+        imm = getImmTypeS(string);
+        rs1 = getRs1(string);
+        rs2 = getRs2(string);
+        instructionAssembly[n] = "SW X" + getDecimal(rs1) + ", X" + getDecimal(rs2) + ", " + getDecimal(imm);
+      case "1100011":// tipo B
+        imm = getImmTypeB(string);
+        rs1 = getRs1(string);
+        rs2 = getRs2(string);
+        if (funct3.compareTo("000") == 0) {// beq
+          instructionAssembly[n] = "BEQ X" + getDecimal(rs1) + ", X" + getDecimal(rs2) + ", " + getDecimal(imm);
+        } else if (funct3.compareTo("001") == 0) {// bne
+          instructionAssembly[n] = "BEQ X" + getDecimal(rs1) + ", X" + getDecimal(rs2) + ", " + getDecimal(imm);
+        }
+      default:
+        System.out.println("Erro.");
+      }
+      n++;
+    }
+  }
+
+  /**
+   * Convert to Binary in Decimal.
+   * 
+   * @param binary Binary
+   * @return Decimal
+   */
+  public static Integer getDecimal(String binary) {
+    return Binary.getInt(Binary.normalizeSize(binary));
+  }
+
+  /**
+   * Extracts the opcode the binary
+   * 
+   * @param binary Binary
+   * @return opcode
+   */
+  public static String getOpcode(String instruction) {
+    return instruction.substring(25, 32);
+  }
+
+  /**
+   * Extracts the funct3 the binary
+   * 
+   * @param binary Binary
+   * @return funct3
+   */
+  public static String getFunct3(String instruction) {
+    return instruction.substring(17, 20);
+  }
+
+  /**
+   * Extracts the funct7 the binary
+   * 
+   * @param binary Binary
+   * @return funct7
+   */
+  public static String getFunct7(String instruction) {
+    return instruction.substring(0, 8);
+  }
+
+  /**
+   * Extracts the rd the binary
+   * 
+   * @param binary Binary
+   * @return rdExtracts the funct3 the binary
+   */
+  public static String getRd(String instruction) {
+    return instruction.substring(20, 25);
+  }
+
+  /**
+   * Extracts the rs1 the binary
+   * 
+   * @param binary Binary
+   * @return rs1
+   */
+  public static String getRs1(String instruction) {
+    return instruction.substring(13, 18);
+  }
+
+  /**
+   * Extracts the rs2 the binary
+   * 
+   * @param binary Binary
+   * @return rs2
+   */
+  public static String getRs2(String instruction) {
+    return instruction.substring(8, 13);
+  }
+
+  /**
+   * Extracts the imm of type i the binary
+   * 
+   * @param binary Binary
+   * @return imm
+   */
+  public static String getImmTypeI(String instruction) {
+    return instruction.substring(0, 13);
+  }
+
+  /**
+   * Extracts the imm of type S the binary
+   * 
+   * @param binary Binary
+   * @return imm
+   */
+  public static String getImmTypeS(String instruction) {
+    return instruction.substring(0, 7) + instruction.substring(20, 25);
+  }
+
+  /**
+   * Extracts the imm of type B the binary
+   * 
+   * @param binary Binary
+   * @return imm
+   */
+  public static String getImmTypeB(String instruction) {
+    return instruction.substring(0, 1) + instruction.substring(24, 25) + instruction.substring(1, 7)
+        + instruction.substring(20, 24);
+  }
+
+}
